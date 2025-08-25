ef4840a49cdf2afcbb60495d5af346999d88978d
GitHub Action <action@github.com>
2025-08-25 06:52:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/tchivs/flinkplatformplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/tchivs/flinkplatformplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.tchivs.flinkplatformplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
